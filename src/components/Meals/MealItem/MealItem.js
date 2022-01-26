a82dfe8c2851ecd0fb71b3f7b62d66d947998cba
Renato Willyan Moratto Filho <renatowmoratto@gmail.com>
2022-01-26 11:02:21 -0300

    feat: MealItem component

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export default function MealItem({ name, description, price }) {
+  const formattedPrice = `$${price.toFixed(2)}`;
+  return (
+    <Meal>
+      <div>
+        <h3>{name}</h3>
+        <Description>{description}</Description>
+        <Price>{formattedPrice}</Price>
+      </div>
+    </Meal>
+  );
+}
+
+const Meal = styled.li`
+  display: flex;
+  justify-content: space-between;
+  margin: 1rem;
+  padding-bottom: 1rem;
+  border-bottom: 1px solid #ccc;
+
+  h3 {
+    margin: 0 0 0.25rem 0;
+  }
+`;
+
+const Description = styled.div`
+  font-style: italic;
+`;
+
+const Price = styled.div`
+  margin-top: 0.25rem;
+  font-weight: bold;
+  color: #ad5502;
+  font-size: 1.25rem;
+`;
